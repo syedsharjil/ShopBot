74d59237fdc129289648cdebd0216e99f4459fe8
Syed Sharjil <syedsharjil@outlook.com>
2020-05-20 07:52:19 +0530

    Removed unwanted comments

--- product_price.py
+++ product_price.py
@@ -2,14 +2,6 @@ from amazonbot import AmazonBot
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-#
-# creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-# client = gspread.authorize(creds)
-# sheet = client.open('ProductPrice').sheet1
-#
-# stuff = sheet.get_all_records()
-# print(stuff)
 class PriceUpdater(object):
 
     def __init__(self, spreadsheet_name):
